d69a85954662de7001b535f27bd21d2dc44d8b15
slmgc <slmgc@ya.ru>
2016-05-24 15:57:30 -0400

    Reorganized actions

--- src/client/actions/posts.js
+++ src/client/actions/posts.js
@@ -1,9 +1,10 @@
 import {RestActions} from 'common/actions'
 const actions = new RestActions('//jsonplaceholder.typicode.com/posts/(:postId)')
 
-actions.getPosts = (dao) =>
-	actions.get(dao).then((posts) => {
-		dao.stateTree.set(['posts', 'items'], posts)
-	})
-
-export default actions
+export default {
+	get(dao) {
+		return actions.get(dao).then((posts) => {
+			dao.stateTree.set(['posts', 'items'], posts)
+		})
+	}
+}
